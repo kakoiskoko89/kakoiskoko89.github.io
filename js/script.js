52d14df088858d0fd4c79d1bfe2b6dd6068ea9ce
kakoiskoko89 <kaoruko160809@gmail.com>
2025-04-30 13:30:54 -0400

    more content pushing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const tulip = document.querySelector("#tulip");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("signup-form");
+    if (form) {
+      form.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+        alert("ありがとう! Welcome to the Japanese Club!");
+      });
+    }
+  });
